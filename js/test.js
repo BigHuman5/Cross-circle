951a8bf3c318d8d4c4ca6445b0e00c8af128ccc2
Human <52690624+BigHuman5@users.noreply.github.com>
2021-12-07 21:28:02 +0600

    Delete test.js

--- js/test.js
+++
@@ -1,9 +0,0 @@
-let params = (new URL(document.location)).searchParams; 
-var type = params.get("type");
-var lvl = params.get("lvl");
-
-$.get('question', {type,lvl}, function(answer){
-
-    answer = JSON.parse(answer);
-    console.log(answer.lvl);
-});
